566a2cddf6eba6f4e308baa54b232c0b988627e7
Trollglodita <1trollglodita@gmail.com>
2014-09-24 09:26:44 -0300

    Socket Emit Events

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ var connect = require('connect'),
     io = require('socket.io');
 
 var app = connect().use(connect.static('public')).listen(3000);
-var socket = io.listen(app);
+var chat_room = io.listen(app);
+
+chat_room.sockets.on('connection', function (socket) {  
+  chat_room.sockets.emit('entrance', {message: 'A new chatter is online.'});
+});
