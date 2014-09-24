28f2f3e30ff3dcf743e18a17ff575c04600ba886
Trollglodita <1trollglodita@gmail.com>
2014-09-24 09:12:07 -0300

    Socket.io Implemented

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
-var connect = require('connect');
+var connect = require('connect'),
+    io = require('socket.io');
 
-var app = connect()
-    .use(connect.bodyParser())
-    .use(connect.static('public'))
-    .use(function (req, res) {
-        if (req.url === '/process') {
-            res.end(req.body.name + ' would repeat ' + req.body.repeat + ' times.');
-        } else {
-            res.end("Invalid Request");
-        }
-    })
-    .listen(3000);
+var app = connect().use(connect.static('public')).listen(3000);
+var socket = io.listen(app);
