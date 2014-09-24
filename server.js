dcfc5e3594694cccb22e33e352cb533a1aa7a45e
Trollglodita <1trollglodita@gmail.com>
2014-09-24 09:31:42 -0300

    Individual Socket Events

--- server.js
+++ server.js
@@ -5,5 +5,11 @@ var app = connect().use(connect.static('public')).listen(3000);
 var chat_room = io.listen(app);
 
 chat_room.sockets.on('connection', function (socket) {  
+  socket.emit('entrance', {message: 'Welcome to the chat room!'});
+
+  socket.on('disconnect', function  () {
+    chat_room.sockets.emit('exit', {message: 'A chatter has disconnected.'});
+  });
+
   chat_room.sockets.emit('entrance', {message: 'A new chatter is online.'});
 });
