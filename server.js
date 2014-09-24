80432d312c705a0ba7d3966d4ea3418b480f4cf8
Trollglodita <1trollglodita@gmail.com>
2014-09-24 10:14:52 -0300

    Using username.id and a Module

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-var connect = require('connect'),
-    io = require('socket.io');
+var io = require('socket.io'),
+  connect = require('connect'),
+  chatter = require('chatter');
 
 var app = connect().use(connect.static('public')).listen(3000);
 var chat_room = io.listen(app);
 
-chat_room.sockets.on('connection', function (socket) {  
-  socket.emit('entrance', {message: 'Welcome to the chat room!'});
+chatter.set_sockets(chat_room.sockets);
 
-  socket.on('disconnect', function  () {
-    chat_room.sockets.emit('exit', {message: 'A chatter has disconnected.'});
+chat_room.sockets.on('connection', function (socket) {
+  chatter.connect_chatter({
+    socket: socket,
+    username: socket.id
   });
-
-  socket.on('chat', function  (data) {
-    chat_room.sockets.emit('chat', {message: '# ' + data.message});
-  });
-
-  chat_room.sockets.emit('entrance', {message: 'A new chatter is online.'});
 });
